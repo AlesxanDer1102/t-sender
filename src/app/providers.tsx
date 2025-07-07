468ed3853a29c005f3c2b898933e8f1f371a0594
AlesxanDer1102 <diego.orrego.t@uni.pe>
2025-07-06 20:21:29 -0500

    feat: agregar react-icons y actualizar configuración de RainbowKit para incluir mainnet

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,17 +1,18 @@
 "use client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { RainbowKitProvider, ConnectButton } from "@rainbow-me/rainbowkit";
+import { RainbowKitProvider } from "@rainbow-me/rainbowkit";
 import { useState, type ReactNode } from "react";
 import { WagmiProvider } from "wagmi";
 import "@rainbow-me/rainbowkit/styles.css";
 import config from "@/rainbowKitConfig";
+
 export function Providers(props: { children: ReactNode }) {
   const [queryClient] = useState(() => new QueryClient());
   return (
     <WagmiProvider config={config}>
       <QueryClientProvider client={queryClient}>
         <RainbowKitProvider>
-          <ConnectButton />
+          
           {props.children}
         </RainbowKitProvider>
       </QueryClientProvider>
---
+++ src/components/Header.tsx
@@ -0,0 +1,16 @@
+import {ConnectButton} from "@rainbow-me/rainbowkit";
+import {FaGithub} from "react-icons/fa";
+
+
+export default function Header(){
+    return(
+        <header>
+            <div>
+                <a href="
+                ">
+                    <FaGithub size={24} />
+                </a>
+            </div>
+        </header>
+    )
+}
